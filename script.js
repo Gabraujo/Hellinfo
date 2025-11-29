2a900b1e00635c8b2bad52e2bb6c7c7ded85f073
Gabraujo <148816574+Gabraujo@users.noreply.github.com>
2025-11-29 00:55:59 -0300

    correção na responsividade do plano de fundo da página principal e dada funções aos botões, criado a pádina dos autômatos

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ ter = document.getElementById("ter");
 ter.onclick = function () {
   window.location.href = "page.html";
 };
+
+aut = document.getElementById("aut");
+aut.onclick = function () {
+  window.location.href = "auto.html";
+};
+
+ilu = document.getElementById("ilu");
+ilu.onclick = function () {
+  window.location.href = "ilum.html";
+};
